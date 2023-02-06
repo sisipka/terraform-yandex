4b22a986fe7728d9e45ccdc664f7bf7a62528756
sisipka <96078556+sisipka@users.noreply.github.com>
2023-02-06 21:55:07 +0300

    create template

---
+++ variables.tf
@@ -0,0 +1,20 @@
+
+variable "token" {
+  default = ""
+}
+
+variable "cloud_id" {
+  default = ""
+}
+
+variable "folder_id" {
+  default = ""
+}
+
+variable "bucket_name" {
+  default = "my_bucket"
+}
+
+variable "zone" {
+  default = "ru-central1-a"
+}
